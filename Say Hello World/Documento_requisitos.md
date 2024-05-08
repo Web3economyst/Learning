f4566f21131ededeaa3cdbbf74c1d3f692584882
Web3economyst <163601250+Web3economyst@users.noreply.github.com>
2024-05-08 04:28:11 -0300

    Add files via upload

---
+++ Say Hello World/Documento_requisitos.md
@@ -0,0 +1,13 @@
+# Documentos requisitos
+
+# Introdução
+
+O problema a ser resolvido por este programa é a que a maquiná faça a impressão da string ''Hello World''
+
+# Desenvolvimento
+
+
+# Conclusão
+
+
+# Referências
---
+++ Say Hello World/HelloWorld.py
@@ -0,0 +1,27 @@
+#--------------------------
+
+#Programa: PrintHelloWorld
+#Descrição: Este programa é para que a maquiná, escreva ''Hello World''
+#Autor: Gabriel Follmer de Mattos
+#Data:08/05/2024
+#Versão: 0.0.1
+
+#--------------------------
+
+
+#Alocação de dados
+
+x = 0
+
+#Entrada de dados
+
+x = ('Hello World')
+
+#Processamento de dados
+
+
+
+#Saída de dados
+
+
+print (x)
