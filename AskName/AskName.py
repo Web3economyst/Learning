17014298460e15e6b249b827fb09c83a3f5a11dc
Web3economyst <163601250+Web3economyst@users.noreply.github.com>
2024-05-08 04:55:12 -0300

    Add files via upload

---
+++ AskName/AskName.py
@@ -0,0 +1,26 @@
+#-------------------------------
+
+#Programa: AskName
+#Descrição: Peça o nome dá pessoa e diga ''olá fulano''
+#Autor: Gabriel Follmer de Mattos
+#Data: 08/05/2024
+#Versão: 0.0.2
+
+#------------------------------
+
+
+#Alocação de memória
+
+name = 0
+
+#Entrada de dados
+
+name = input('Qual é o seu nome?)
+
+#Processamento de dados
+
+name
+
+#Saída de dados
+
+print (f' Oi {name}')
---
+++ AskName/AskName_documentos.md
@@ -0,0 +1,12 @@
+# Documentos requisitos
+
+# Introdução
+Este programa será usado para pedir o nome das pessoas e as cumprimentar com a frase ''Oi fulano''.
+
+# Desenvolvimento
+
+
+# Conclusão
+
+
+# Revisão
